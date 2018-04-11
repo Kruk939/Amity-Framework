488b9bd4dedbe95dfc4771990be792bbc0b2968b
Kruk939 <kajetan@smsnet.pl>
2018-04-11 13:52:52 +0200

    Prepare

---
+++ Amity_Mission.amityisland/Functions/Modules/Crafting/Functions.hpp
@@ -0,0 +1,16 @@
+class ClientModules_Crafting {
+      class Init {
+            file="Functions\Modules\Crafting\Functions\Init";
+            class initEventHandlers {};
+            class initListeners {};
+            class initMenu {};
+            class initPlayer {};
+            class initVariables {};
+      };
+      class Misc {
+            file="Functions\Modules\Crafting\Functions\Misc";
+      };
+      class Dialog {
+            file="Functions\Modules\Crafting\Functions\Dialog";
+      };
+};
---
+++ Amity_Server/Functions/Modules/Crafting/Functions.hpp
@@ -0,0 +1,9 @@
+class ServerModules_Shop {
+      class Init {
+            file = "Amity_Server\Functions\Modules\Crafting"
+            class init {};
+      };
+      class Functions {
+            file = "Amity_Server\Functions\Modules\Crafting\Functions\Misc"
+      };
+};
