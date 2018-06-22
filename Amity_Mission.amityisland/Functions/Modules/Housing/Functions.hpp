fc97c02082ebb308eaa4f7959f5a952d47c10edb
Kruk939 <kajetan@smsnet.pl>
2018-06-22 18:04:23 +0200

    Asd

--- Amity_Mission.amityisland/Functions/Modules/Housing/Functions.hpp
+++ Amity_Mission.amityisland/Functions/Modules/Housing/Functions.hpp
@@ -7,4 +7,21 @@ class ClientModules_Housing {
             class initPlayer {};
             class initVariables {};
       };
+      class Misc {
+            file="Functions\Modules\Housing\Functions\Misc";
+            class isCargo {};
+            class isFurniture {};
+      };
+      class Dialog {
+            file="Functions\Modules\Housing\Functions\Dialog";
+            class buy_action {};
+            class buy_open {};
+            class buy_receive {};
+            class manage_action {};
+            class manage_open {};
+            class manage_receive {};
+            class sell_action {};
+            class sell_open {};
+            class sell_receive {};
+      };
 };
